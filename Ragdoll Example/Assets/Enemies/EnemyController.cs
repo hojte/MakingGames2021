5ed78641259f7a4968573c7d098c4eb4cd3b2e8f
Frederik Pil <frpi@itu.dk>
2021-11-05 18:35:23 +0100

    added freds unity project to main

---
+++ Ragdoll Example/Assets/Enemies/EnemyController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        setRigidBodyState(true);
+        setColliderState(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player") {
+            die();
+        }
+       
+    }
+
+    void die()
+    {
+        Destroy(gameObject, 7f);
+        GetComponent<Animator>().enabled = false;
+        setRigidBodyState(false);
+        setColliderState(true);
+
+    }
+    void setRigidBodyState(bool state)
+    {
+        Rigidbody[] rigidbodies = GetComponentsInChildren<Rigidbody>();
+        foreach (Rigidbody rigidbody in rigidbodies) {
+            rigidbody.isKinematic = state;
+        }
+        GetComponent<Rigidbody>().isKinematic = !state;
+    }
+
+    void setColliderState(bool state) {
+        Collider[] colliders = GetComponentsInChildren<Collider>();
+
+        foreach (Collider collider in colliders) {
+            collider.enabled = state;
+        }
+        GetComponent<Collider>().enabled = !state;
+    }
+}
---
+++ Ragdoll Example/Assets/PlayerMovement.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+
+    public float speed = 6f; 
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    Vector3 velocity;
+    public float gravity = -9.81f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    bool isGrounded;
+
+    void Update()
+    {
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f) {
+
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f)* Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+
+            
+        }
+
+        //Gravity
+        if (isGrounded && velocity.y <0) {
+            velocity.y = -2f;
+        }
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+    }
+
+
+    public float pushPower = 2.0F;
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        
+        if (hit.collider.gameObject.tag == "Box" || hit.collider.gameObject.tag == "Ball") {
+
+            Rigidbody body = hit.collider.attachedRigidbody;
+
+            // no rigidbody
+            if (body == null || body.isKinematic)
+                return;
+
+            // We dont want to push objects below us
+            if (hit.moveDirection.y < -0.3f)
+                return;
+
+            // Calculate push direction from move direction,
+            // we only push objects to the sides never up and down
+            Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+
+            // If you know how fast your character is trying to move,
+            // then you can also multiply the push velocity by that.
+
+            // Apply the push
+            body.velocity = pushDir * pushPower;
+
+
+        }
+        
+    }
+
+}
