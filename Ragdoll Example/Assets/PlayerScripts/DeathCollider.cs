37c5edb33c37b426b00fdee840b9f68b6e031d51
fpil <pilfrederik@gmail.com>
2021-11-16 17:14:36 +0100

    Added player death script

---
+++ Ragdoll Example/Assets/PlayerScripts/DeathCollider.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class DeathCollider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            
+            Debug.Log("Collision with enemy");
+            playerDie(GameObject.FindWithTag("PlayerAnimator"), GameObject.FindWithTag("Player"));
+        }
+
+        
+    }
+    
+    void playerDie(GameObject playerAnimator, GameObject player)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        
+
+        //Destroy(gameObject, 7f);
+        // player.GetComponent<CapsuleCollider>().enabled = false;
+        //player.GetComponent<CharacterController>().enabled = false; 
+
+        //playerAnimator.GetComponent<Animator>().enabled = false;
+
+
+        //gameObject.GetComponent<NavMeshAgent>().enabled = false;
+        //setRigidBodyState(false);
+        //setColliderState(true);
+    }
+}
