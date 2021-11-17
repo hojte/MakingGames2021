11d56488b47649d8c7a5cc79ee886aac9051df26
mhooITU <mhoo@itu.dk>
2021-11-17 18:49:30 +0100

    move pickup responsibility to player controller.

---
+++ Ragdoll Example/Assets/Interactions/PickupController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using PlayerScripts;
+using UnityEngine;
+
+public class PickupController : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        try
+        { // todo might lag??
+            other.GetComponent<PlayerController>().pickupables.Add(GetComponent<Rigidbody>());
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        try
+        {
+            other.GetComponent<PlayerController>().pickupables.Remove(GetComponent<Rigidbody>());
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
--- Ragdoll Example/Assets/PlayerScripts/PlayerController.cs
+++ Ragdoll Example/Assets/PlayerScripts/PlayerController.cs
@@ -1,16 +1,63 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace PlayerScripts
 {
     public class PlayerController : MonoBehaviour
     {
-        public Rigidbody throwable = null;
-        public Vector3 throwablePosition;
+        public HashSet<Rigidbody> pickupables = new HashSet<Rigidbody>();
+        private Rigidbody throwSlot;
+        private Vector3 throwablePosition;
+        private Transform _mainCam;
+
+        private void Start()
+        {
+            _mainCam = GameObject.FindGameObjectWithTag("MainCamera").transform;
+        }
+
         void Update()
         {
-            throwablePosition = GetComponent<Transform>().position;
+            throwablePosition = transform.position;
             throwablePosition.y += 5;
+            
+            if (throwSlot && !Input.GetKeyDown(KeyCode.E))
+            { // Update position of filled throwSlot
+                var playerPos = transform.position;
+                throwSlot.transform.position = new Vector3(playerPos.x, playerPos.y+5, playerPos.z);
+                throwSlot.angularVelocity = Vector3.zero;
+                throwSlot.rotation = Quaternion.LookRotation(_mainCam.forward, _mainCam.up);
+            }
+            else if (throwSlot && Input.GetKeyDown(KeyCode.E))
+            { // Throw Item
+                throwSlot.velocity = throwSlot.transform.TransformDirection(Vector3.forward * 30);
+                throwSlot = null;
+            }
+            else if (Input.GetKeyDown(KeyCode.E))
+            {
+                print("trying to take an item...");
+                print(TryTakeNearbyItem() ? "woo! item taken!" : "couldn't take an item");
+            }
+        }
+        private bool TryTakeNearbyItem()
+        {
+            if (throwSlot != null) return false;
+            Rigidbody finalPickup = null;
+            foreach (Rigidbody pickup in pickupables)
+            {
+                if (!finalPickup) finalPickup = pickup;
+                if (Vector3.Distance(pickup.transform.position, transform.position) <
+                    Vector3.Distance(finalPickup.transform.position, transform.position))
+                    finalPickup = pickup;
+            }
+
+            if (finalPickup != null)
+            {
+                finalPickup.transform.position = throwablePosition;
+                throwSlot = finalPickup;
+                return true;
+            }
+            return false;
         }
     }
-    
 }
