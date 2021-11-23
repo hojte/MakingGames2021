761a34c2f5eeccf5154c66080e84c6c07b34a4e6
fpil <pilfrederik@gmail.com>
2021-11-23 16:07:09 +0100

    Added scene loader

---
+++ Ragdoll Example/Assets/NextRoomTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextRoomTrigger : MonoBehaviour
+{
+    public String sceneToLoad; 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+        
+    }
+}
