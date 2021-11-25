a1edbb6682de6c594a05495030e45417739d07bc
mhooITU <mhoo@itu.dk>
2021-11-25 09:40:52 +0100

    sound implementation + assets

---
+++ Ragdoll Example/Assets/Sound/AudioManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+namespace Sound
+{
+    public class AudioManager : MonoBehaviour
+    {
+        public Sound[] sounds;
+        void Awake()
+        {
+            foreach (var s in sounds)
+            {
+                s.source = gameObject.AddComponent<AudioSource>();
+                s.source.clip = s.clip;
+            }
+        }
+
+        private void Start()
+        {
+            Play("MainTheme", true);
+        }
+
+        void Play(string soundName, bool loop)
+        {
+            Sound s = Array.Find(sounds, sound => sound.name == soundName);
+            s.source.volume = s.volume;
+            s.source.loop = loop;
+            s.source.Play();
+        }
+        void Stop(string soundName)
+        {
+            Sound s = Array.Find(sounds, sound => sound.name == soundName);
+            s.source.Stop();
+        }
+    }
+}
---
+++ Ragdoll Example/Assets/Sound/Sound.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace Sound
+{
+    [Serializable]
+    public class Sound
+    {
+        public string name;
+        public AudioClip clip;
+        [Range(0, 1f)] public float volume = .5f;
+        [HideInInspector] public AudioSource source;
+    }
+}
