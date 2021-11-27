395cfa30bdd1b547cf96640142953c9c39a5a8c9
mhooITU <mhoo@itu.dk>
2021-11-27 16:13:54 +0100

    some cleanup

--- Ragdoll Example/Assets/PlayerScripts/PlayerController.cs
+++ Ragdoll Example/Assets/PlayerScripts/PlayerController.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Interactions;
+﻿using Interactions;
 using Sound;
-using UI;
 using UnityEditor;
 using UnityEngine;
 
