74808a1c0bf6c279187a735683ccf193c9cb2cce
mhooITU <mhoo@itu.dk>
2021-11-27 13:11:55 +0100

    add shop prefab

---
+++ Ragdoll Example/Assets/Interactions/Shop/ShopController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ShopController : MonoBehaviour
+{
+    public List<ShopItemController> shopItems;
+    // Start is called before the first frame update
+    void Start()
+    {
+        shopItems = GetComponentsInChildren<ShopItemController>().ToList();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
