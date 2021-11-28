afcb74e0add535cf5241ffb56384ec552a533146
fpil <pilfrederik@gmail.com>
2021-11-28 15:17:49 +0100

    cleaned up code

--- Ragdoll Example/Assets/ConveyorBelt.cs
+++ Ragdoll Example/Assets/ConveyorBelt.cs
@@ -15,26 +15,20 @@ public class ConveyorBelt : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(onBelt.Count);
         for (int i = 0; i < onBelt.Count; i++)
         {
             onBelt[i].transform.position -= transform.right * Time.deltaTime * speed; 
         }
-        
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Collision belt");
-        //collision.gameObject.GetComponent<Rigidbody>().freezeRotation = true;
         collision.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionX;
-        //collision.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionZ;
         onBelt.Add(collision.gameObject);
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        //collision.gameObject.GetComponent<Rigidbody>().freezeRotation = false; 
         collision.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
         onBelt.Remove(collision.gameObject);
     }
