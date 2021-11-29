d3cda6dad6315a241022e0dc900460471c57640f
mhooITU <mhoo@itu.dk>
2021-11-29 23:54:06 +0100

    implement shop balance

--- Ragdoll Example/Assets/UI/ScoreController.cs
+++ Ragdoll Example/Assets/UI/ScoreController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using TMPro;
+﻿using TMPro;
 using UnityEngine;
 
 namespace UI
@@ -7,7 +6,7 @@ namespace UI
     public class ScoreController : MonoBehaviour
     {
         public int playerScore;
-        public int scoreSpent;
+        public int shopBalance;
         private TextMeshProUGUI uiText;
         float m_AccumulatedDeltaTime = 0f;
         int m_AccumulatedFrameCount = 0;
@@ -19,13 +18,14 @@ namespace UI
 
         private void Update()
         {
+            if (playerScore < 0) playerScore = 0; // never negative score
             m_AccumulatedDeltaTime += Time.deltaTime;
             m_AccumulatedFrameCount++;
 
             if (m_AccumulatedDeltaTime >= 0.5f) // time to update Score board
             {
                 int framerate = Mathf.RoundToInt((float)m_AccumulatedFrameCount / m_AccumulatedDeltaTime);
-                uiText.text = "Score: "+playerScore+ "("+GetShopFunds()+")"+
+                uiText.text = "Score: "+playerScore+ "("+shopBalance+")"+
                               "\nFramerate: "+ framerate;
 
                 m_AccumulatedDeltaTime = 0f;
@@ -35,37 +35,37 @@ namespace UI
 
         public void EnemyKilled()
         {
-            playerScore += 5;
+            playerScore += 3;
+            shopBalance += 3;
         }
         public void PlayerDied()
         {
             playerScore -= 2;
+            shopBalance -= 2;
         }
         public void Pickup(bool increment)
         {
-            playerScore += increment?5:-3;
+            playerScore += increment?4:-3;
+            shopBalance += increment?4:-3;
         }
         public void LevelCompleted()
         {
-            playerScore += 15;
-        }
-
-        public int GetShopFunds()
-        {
-            return playerScore - scoreSpent;
+            playerScore += 8;
+            shopBalance += 8;
         }
 
         public bool BuyAmount(int amount)
         {
-            if (amount > GetShopFunds()) return false;
+            if (amount > shopBalance) return false;
 
-            scoreSpent += amount;
+            shopBalance-=amount;
             return true;
         }
 
         public void PlayerStunned()
         {
             playerScore -= 2;
+            shopBalance -= 2;
         }
     }
 }
