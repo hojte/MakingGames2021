2c593d69a6087d49fdb5308c89fd5ac5b9eb76c8
javiercdag <javiercdag@gmail.com>
2021-11-29 20:33:50 +0100

    Pressure Plates - To Do improve detection of player

---
+++ Ragdoll Example/Assets/Environment/ppLinker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Interactions;
+
+public class ppLinker : MonoBehaviour
+{
+    //What to do when the linked PP is being pressured
+    public void startOfInteraction()
+    {
+        // Example of use:
+        if (gameObject.GetComponent<DoorController>())
+        {
+            gameObject.GetComponent<DoorController>().SetClosed(false);
+        }
+    }
+
+    // What to do when the linked PP is released
+    public void endOfInteraction()
+    {
+        // Example of use:
+        if (gameObject.GetComponent<DoorController>())
+        {
+            gameObject.GetComponent<DoorController>().SetClosed(true);
+        }
+    }
+}
---
+++ Ragdoll Example/Assets/PressurePlate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Interactions;
+
+public class PressurePlate : MonoBehaviour
+{
+    public GameObject linkedGameObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Renderer>().material.color = Color.gray;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        linkedGameObject.GetComponent<ppLinker>().startOfInteraction();
+        GetComponent<Renderer>().material.color = Color.green;
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        linkedGameObject.GetComponent<ppLinker>().endOfInteraction();
+        GetComponent<Renderer>().material.color = Color.gray;
+    }
+}
