246cb50caf90ef85f44993cb68219790841e64a8
fpil <pilfrederik@gmail.com>
2021-12-02 12:27:34 +0100

    Added light to boss level

---
+++ Ragdoll Example/Assets/LightController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+
+
+    public Light myLight;
+    public float pulseSpeed = 1f; //this is in seconds
+    private float timer;
+
+    void Start()
+    {
+        //myLight = GetComponent<Light>();
+    }
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > pulseSpeed)
+        {
+            Debug.Log("Puls");
+            timer = 0;
+            myLight.enabled = !myLight.enabled;
+            //myLight.enabled = false;
+        }
+    }
+}
