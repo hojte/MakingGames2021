c8700ff21feb574b59f12e88a1d831f5d2f0f904
fpil <pilfrederik@gmail.com>
2021-12-02 13:03:14 +0100

    Added alarm lights to forklift

--- Ragdoll Example/Assets/LightController.cs
+++ Ragdoll Example/Assets/LightController.cs
@@ -6,23 +6,23 @@ public class LightController : MonoBehaviour
 {
 
 
-    public Light myLight;
-    public float pulseSpeed = 1f; //this is in seconds
-    private float timer;
-
-    void Start()
-    {
-        //myLight = GetComponent<Light>();
-    }
+    public Light myLight1;
+    public Light myLight2;
+    public Light point1;
+    public float pulseSpeed;
+    public float speed;
+    public float timer; 
     void Update()
     {
+        myLight1.transform.Rotate(0, speed*Time.deltaTime, 0);
+        myLight2.transform.Rotate(0, -speed * Time.deltaTime, 0);
+
         timer += Time.deltaTime;
         if (timer > pulseSpeed)
         {
-            Debug.Log("Puls");
             timer = 0;
-            myLight.enabled = !myLight.enabled;
-            //myLight.enabled = false;
+            point1.enabled = !point1.enabled;
         }
+
     }
 }
