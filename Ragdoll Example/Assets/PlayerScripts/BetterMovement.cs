2d2def601fedd8af607cc2b4d94d6ac66b77edfc
fpil <pilfrederik@gmail.com>
2021-12-05 19:39:41 +0100

    Added slide on conveyorbelts

--- Ragdoll Example/Assets/PlayerScripts/BetterMovement.cs
+++ Ragdoll Example/Assets/PlayerScripts/BetterMovement.cs
@@ -125,7 +125,7 @@ public class BetterMovement : MonoBehaviour
 
                 if (slideTimerTrigger < 0.0f)
                 {
-                    if (isCrouching && groundedPlayer && !onBelt)
+                    if (isCrouching && (groundedPlayer || onBelt))
                     {
                         if (!GetComponent<AudioSource>())
                         {
@@ -147,7 +147,7 @@ public class BetterMovement : MonoBehaviour
             }
 
             //Sliding
-            if (isSliding && groundedPlayer)
+            if (isSliding && (groundedPlayer || onBelt))
             {
                 gameObject.transform.eulerAngles = new Vector3(
                     -85,
