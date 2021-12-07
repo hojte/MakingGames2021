0d350265da04f0a4a0e18ac1e758b7242296c55e
javiercdag <javiercdag@gmail.com>
2021-12-07 17:21:58 +0100

    wip boss door

---
+++ Ragdoll Example/Assets/doorClosing.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorClosing : MonoBehaviour
+{
+    public GameObject linkedObject;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<BetterMovement>()){
+            linkedObject.transform.localRotation = Quaternion.Euler(0, 0, 0);
+            Debug.Log("immad");
+        }
+    }
+}
